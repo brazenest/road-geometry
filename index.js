389c7105fa1d4504f43b115f116202e22183c58e
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2025-07-31 18:06:08 -0600

    Update index.js
    Temporarily disabled use of --radius flag, as side friction value is now parameterized and currently lacks a parameterization for radius values (velocity values only).

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const USER_FLAGS = Object.keys(USER_ARGS)
 
 if (!USER_FLAGS.includes('--velocity') && !USER_FLAGS.includes('--radius')) createErrorMessage('Must specify a radius and/or a velocity/speed.')
 if (USER_FLAGS.includes('--velocity') && !(USER_ARGS['--velocity'] > 0)) createErrorMessage('Velocity must be a positive integer.')
-if (USER_FLAGS.includes('--radius') && !(USER_ARGS['--radius'] > 0)) createErrorMessage('Radius must be a positive integer.')
+if (USER_FLAGS.includes('--radius') createErrorMessage('The --radius flag is temporarily disabled. Use --velocity instead.')
+// if (USER_FLAGS.includes('--radius') && !(USER_ARGS['--radius'] > 0)) createErrorMessage('Radius must be a positive integer.')
 
 const R = USER_FLAGS.includes('--radius') ? USER_ARGS['--radius'] : rForGivenV(USER_ARGS['--velocity'])
 const V = USER_FLAGS.includes('--velocity') ? USER_ARGS['--velocity'] : vForGivenR(USER_ARGS['--radius'])
